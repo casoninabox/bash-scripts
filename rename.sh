d3a1f941e9def232d8a3860f07d81c65e11e047a
Cason Clagg <cason@cason.cc>
2015-04-19 02:10:25 -0500

    init

---
+++ rename.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -o errexit
+shopt -s nullglob
+shopt -s nocaseglob
+
+# Allow Spaces
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+MYPATH=$1
+FILEEXTS=$2
+
+# Avoid overriding same filenames
+COUNTER=1
+cd $MYPATH
+for i in $(ls *.$FILEEXTS -rt); do
+	filename="${i##*/}"
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+
+    mv "$i" $COUNTER-rename-temp.$extension
+    echo "Temped $i to $COUNTER-rename-temp.$extension"
+    let COUNTER=COUNTER+1 
+done
+
+# Start numbering at 1
+COUNTER=1
+for j in $(ls *.$FILEEXTS -rt); do
+	filename="${j##*/}"
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+
+	COUNTERSTRING=$(printf %06d.%s ${X%.*} $COUNTER)
+    mv "$j" $COUNTERSTRING$extension
+    echo "Renamed $j to $COUNTERSTRING$extension"
+    let COUNTER=COUNTER+1 
+done
+
+# Reset IFS
+IFS=$SAVEIFS
+
+# Copy Logo
+cd ..
+cp logo.png $MYPATH/logo.png
+echo "Copied Logo"
